e85db85221679e10cdce27f612e3e920eb750e70
David Waka <wakulewicz@hotmail.com>
2019-09-17 15:47:36 -0400

    DB integration

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -1,6 +1,19 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, g
+import pymysql
+
+def get_db():
+    if 'db' not in g:
+        g.db = pymysql.connect("aliasMeToDB","viper","viper67","IXPlus")
+
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
 
 def create_app(test_config=None):
     # create and configure the app
@@ -31,6 +44,22 @@ def create_app(test_config=None):
         if not segment or not pid:
             return "no segment or no pid", 400
         else:
+            add_PID_segment(pid, segment)
             return "{}, {}".format(segment, pid)
 
     return app
+
+def add_PID_segment(pid, segmentID):
+    db = get_db()
+    cursor = db.cursor()
+
+    sql = "INSERT INTO PIDSegmentMappings(PID, segmentID, count) \
+           VALUES('%s', '%s', 1) \
+           ON DUPLICATE KEY UPDATE count = count + 1" % \
+                   (pid, segmentID)
+
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
