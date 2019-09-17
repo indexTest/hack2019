3574e8598cc3216b0f49549cf7e212b3754322ff
tony.mei <tony.mei@indexexchange.com>
2019-09-17 15:03:20 -0400

    added stuff

--- server/server.py
+++ server/server.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
  
-from flask import Flask, json
+from flask import Flask, json, request
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
 api = Flask(__name__)
 
-@api.route('/companies', methods=['GET'])
-def get_companies():
-  return json.dumps(companies)
+@api.route('/addsegment', methods=['GET'])
+def addSegment():
+	segment = request.args.get('segment')
+	pid     = request.args.get('pid')
+	
 
 if __name__ == '__main__':
-    api.run()
+	api.run()
