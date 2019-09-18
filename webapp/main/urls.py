eed67702d1b6dfa3a75936c6a30af8c99466228e
shelly.chen <shelly.chen@indexexchange.com>
2019-09-17 20:00:16 -0400

    modify url

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -25,9 +25,9 @@ urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("/", views.homepage, name="homepage"),
     # path("internet/register/", views.register, name="register"),
-    path("signup/", views.signup, name="signup"),
+    path("/internet/signup/", views.signup, name="signup"),
     # path("internet/logout/", views.logout_request, name="logout"),
     # path("internet/login/", views.login_request, name="login"),
-    path("misc1/", views.misc1, name="misc1"),
-    path("misc2/", views.misc2, name="misc2"),
+    path("/internet/misc1/", views.misc1, name="misc1"),
+    path("/internet/misc2/", views.misc2, name="misc2"),
 ]
