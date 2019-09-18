eb8444d806d88b9354844f8d5ec8aa9e341cd538
shelly.chen <shelly.chen@indexexchange.com>
2019-09-17 20:28:32 -0400

    5

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -22,9 +22,9 @@ from django.conf.urls import url
 app_name = "main"
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("/", views.homepage, name="homepage"),
-    path("signup/", views.signup, name="signup"),
-    path("misc1/", views.misc1, name="misc1"),
-    path("misc2/", views.misc2, name="misc2"),
+    path("internet", views.homepage, name="homepage"),
+    path("internet/", views.homepage, name="homepage"),
+    path("internet/signup/", views.signup, name="signup"),
+    path("internet/misc1/", views.misc1, name="misc1"),
+    path("internet/misc2/", views.misc2, name="misc2"),
 ]
