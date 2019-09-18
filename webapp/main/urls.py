194e0a17e48ebe6b1331b1f0affe7c491d03ef53
shelly.chen <shelly.chen@indexexchange.com>
2019-09-17 20:53:35 -0400

    11

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -24,8 +24,7 @@ app_name = "main"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("/", views.homepage, name="homepage"),
-    path("internet/signup/", views.signup, name="signup"),
     path("signup/", views.signup, name="signup"),
-    path("misc1/", views.misc1, name="misc1"),
-    path("misc2/", views.misc2, name="misc2"),
+    path("signup/misc1/", views.misc1, name="misc1"),
+    path("signup/misc2/", views.misc2, name="misc2"),
 ]
